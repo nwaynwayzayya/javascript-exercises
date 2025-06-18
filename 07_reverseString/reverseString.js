6a50cfaa249651a8bf47967e554250f0b0e685bd
nwaynwayzayya <6631503081@lamduan.mfu.ac.th>
2025-06-18 15:13:41 +0700

    Do exercises 06 to 11

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let splitted = string.split("");
+    let result = "";
+    for (let i=splitted.length-1; i>=0; i--){
+        result += splitted[i];
+    }
+    return result;
 };
 
 // Do not edit below this line

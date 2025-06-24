d1844afcaa68175adcd9d3189b354a9dbea47f77
nwaynwayzayya <6631503081@lamduan.mfu.ac.th>
2025-06-24 17:29:39 +0700

    Do exercises 12-16

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    num = Number(num);
 
+    if (num < 0) return "OOPS";
+
+    if (num === 0) return 0;
+
+    let a = 0;
+    let b = 1;
+
+    for (i=1; i<num; i++) {
+        const temp = b;
+        b = a + b;
+        a = temp;
+    }
+
+    return b;   
 };
 
 // Do not edit below this line
